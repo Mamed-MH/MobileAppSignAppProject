b0a5bdfe558e304650488629da542e3df77edbef
Mamed-MH <muhammedmz0619@gmail.com>
2024-10-02 20:30:04 +0400

    Initial Commit

---
+++ MobileAppSignApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileAppSignApp
+//
+//  Created by Mamed Hacıyev on 2.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MobileAppSignAppTests/MobileAppSignAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAppSignAppTests.swift
+//  MobileAppSignAppTests
+//
+//  Created by Mamed Hacıyev on 2.10.2024.
+//
+
+import Testing
+@testable import MobileAppSignApp
+
+struct MobileAppSignAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
